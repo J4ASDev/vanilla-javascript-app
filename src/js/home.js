948112d66bcf4beb777cb73f122ac5d5b03462d9
Jonathan Araujo <j4asdev@gmail.com>
2019-02-14 17:10:11 -0430

    [Added] Handlers events: 'Form Submit' and 'Modal Click'

--- src/js/home.js
+++ src/js/home.js
@@ -2,6 +2,19 @@
 (async function load(){
   const API = 'https://api.audioboom.com/'
 
+  /* Elements DOM */
+  const home = document.getElementById('home')
+
+  const modal = document.getElementById('modal')
+  const overlay = document.getElementById('overlay')
+  
+  const form = document.getElementById('form')
+  const featuring = document.getElementById('featuring')
+
+  const basicContainer = document.getElementById('basicContainer')
+  const popularContainer = document.getElementById('popularContainer')
+  const featuredContainer = document.getElementById('featuredContainer')
+
   /* Function get data api Audioboom */
   async function getData(URL) {
     const result = await fetch(`${API}${URL}`)
@@ -10,28 +23,16 @@
     return audio_clips
   }
   
-  /* Data Api Audioboom */
-  const basicPodcasts = await getData('audio_clips')
-  const popularPodcasts = await getData('audio_clips/popular')
-  const featuredPodcasts = await getData('audio_clips/featured')
-
-  /* Elements DOM */
-  const home = document.getElementById('home')
-  const modal = document.getElementById('modal')
-
-  const basicContainer = document.getElementById('basicContainer')
-  const popularContainer = document.getElementById('popularContainer')
-  const featuredContainer = document.getElementById('featuredContainer')
-  
   /* Functions to manipulate the HTML (create and add) */
   const HTMLTemplate = podcasts => {
     return (`
-      <div>
+      <div class="podcasts-container">
         <figure>
-          ${ /* <img 
+          <img
+            class="podcasts-cover"
             src="${podcasts.urls.image || podcasts.channel && podcasts.channel.urls.logo_image.original}" 
             alt="${podcasts.title}"
-          > */ ''}
+          >
         </figure>
         <h4>${podcasts.title}</h4>
       </div>
@@ -46,24 +47,45 @@
   }
 
   const renderTemplate = (list, container) => {
-    container.querySelector('img').remove()
+    if (container.querySelector('img')) container.querySelector('img').remove()
 
     list.forEach(podcasts => {
       const HTMLString = HTMLTemplate(podcasts)
       const podcastsElement = createTemplate(HTMLString)
          
       container.append(podcastsElement)
+      
+      podcastsElement.addEventListener('click', () => {
+
+        console.log(podcastsElement)
+        modal.style.display = 'block'
+      })
     })
   }
-  
-  debugger
-  
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const data = new FormData(form)
+    const str = data.get('search')
+    const value = str.replace(/ /g, "-")
+    
+    const searchTopic = await getData(`audio_clips?find[query]=${value}`)
+    featuring.classList.add('enabled')
+
+    renderTemplate(searchTopic, featuring)
+  })
+
+
+  /* Data Api Audioboom */
+  const basicPodcasts = await getData('audio_clips')
+  const popularPodcasts = await getData('audio_clips/popular')
+  const featuredPodcasts = await getData('audio_clips/featured')
+
   /* Render dates */
   renderTemplate(basicPodcasts, basicContainer)
   renderTemplate(popularPodcasts, popularContainer)
   renderTemplate(featuredPodcasts, featuredContainer)
   
-  debugger
 })()
 
 
